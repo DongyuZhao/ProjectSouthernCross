16e62a8f1870182509ddd5d890bb0f7210101b88
DongyuZhao <dy.zhao@outlook.com>
2016-05-11 22:04:14 +1000

    Remove the optional value of span in SyntaxTrivia

--- src/project/southern_cross/code_analysis/SyntaxTrivia.java
+++ src/project/southern_cross/code_analysis/SyntaxTrivia.java
@@ -6,14 +6,21 @@ import java.util.Optional;
  * Created by Dy.Zhao on 2016/5/11 0011.
  */
 public class SyntaxTrivia {
-    private Optional<Span> _span = Optional.of(new Span());
+    private Span _span = new Span();
     private String _rawString;
 
-    public Optional<Span> span() {
+    public SyntaxTrivia(String rawString, int spanStart, int spanEnd) {
+        this._rawString = rawString;
+        this._span = new Span(spanStart, spanEnd);
+    }
+
+    public Span span() {
         return _span;
     }
 
     public String rawString() {
         return _rawString;
     }
+
+
 }
