8ed2ef040491d743d83a478cce0469102bdbf8f7
Junjie <xxxxujunjie@gmail.com>
2016-05-17 22:05:57 +1000

    usecase

---
+++ src/project/southern_cross/test/TokenizerTest.java
@@ -0,0 +1,14 @@
+package project.southern_cross.test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 17/05/2016.
+ */
+public class TokenizerTest {
+    @org.junit.Test
+    public void tokenize() throws Exception {
+
+    }
+
+}
