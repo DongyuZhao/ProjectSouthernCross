243f401d050da9822f134f1458025073697151b9
Dy.Zhao <dy.zhao@outlook.com>
2016-08-02 19:06:47 +1000

    change the parse method in the tree.

--- src/main/java/project/southern_cross/code_analysis/tweet_ql/TweetQlSyntaxTree.java
+++ src/main/java/project/southern_cross/code_analysis/tweet_ql/TweetQlSyntaxTree.java
@@ -3,6 +3,9 @@ package project.southern_cross.code_analysis.tweet_ql;
 import project.southern_cross.code_analysis.core.ParsedSyntaxTree;
 import project.southern_cross.code_analysis.core.SyntaxNode;
 import project.southern_cross.code_analysis.core.SyntaxTree;
+import project.southern_cross.code_analysis.core.exceptions.UnsupportedLanguageException;
+
+import java.util.concurrent.TimeoutException;
 
 /**
  * Project Southern Cross
@@ -14,10 +17,19 @@ public class TweetQlSyntaxTree {
 
     private ParsedSyntaxTree parsedSyntaxTree;
 
-    public static TweetQlSyntaxTree parse(String source) {
-        TweetQlSyntaxTree result = new TweetQlSyntaxTree();
-        result.parsedSyntaxTree = SyntaxTree.parse(source, "tweet_ql");
-        return result;
+    public static TweetQlSyntaxTree parse(String source) throws TimeoutException, UnsupportedLanguageException {
+        try {
+            TweetQlSyntaxTree result = new TweetQlSyntaxTree();
+            ParsedSyntaxTree parsedSyntaxTree = SyntaxTree.parse(source, "tweet_ql");
+            if (parsedSyntaxTree != null) {
+                result.parsedSyntaxTree = parsedSyntaxTree;
+                return result;
+            }
+        } catch (TimeoutException | UnsupportedLanguageException e) {
+            e.printStackTrace();
+            throw e;
+        }
+        return null;
     }
 
     public ParsedSyntaxTree getParsedSyntaxTree() {
