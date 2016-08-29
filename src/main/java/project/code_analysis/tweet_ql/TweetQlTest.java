37558d38bca6588310e7e747be2004202bc2adf3
DongyuZhao <dy.zhao@outlook.com>
2016-08-29 18:10:51 +1000

    test JitPack

---
+++ src/main/java/project/code_analysis/tweet_ql/TweetQlTest.java
@@ -0,0 +1,10 @@
+package project.code_analysis.tweet_ql;
+
+/**
+ * This is a open source project provided as-is without any
+ * guarantee.
+ * <p>
+ * Created by Dy.Zhao on 2016/8/29.
+ */
+public class TweetQlTest {
+}
